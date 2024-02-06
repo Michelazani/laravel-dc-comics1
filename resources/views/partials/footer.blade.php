46fcde71b5aac52a297393a918ca7f38ad87aa42
Michela Zani <145712045+Michelazani@users.noreply.github.com>
2024-02-06 15:27:56 +0100

    Initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,75 @@
+<footer>
+    <section class="upper-footer">
+        <div>
+            <article id="dc-comics">
+                <h2>
+                    dc comics
+                </h2>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+
+                </ul>
+            </article>
+            <article id="shop">
+                <h2>
+                    shop
+                </h2>
+                <ul>
+                    <li><a href="#">Shop dc</a></li>
+                    <li><a href="#">Shop dc collectibles</a></li>
+                </ul>
+            </article>
+        </div>
+
+        <article id="dc">
+            <h2>
+                dc
+            </h2>
+            <ul>
+                <li><a href="#">Term Of Use</a></li>
+                <li><a href="#">Privacy policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </article>
+        <article id="sites">
+            <h2>
+                sites
+            </h2>
+            <ul>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">Dc Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+            </ul>
+        </article>
+        <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="dc logo background">
+
+    </section>
+    <section class="lower-footer">
+        <button> Sign up now!</button>
+        <div>
+            <h2>follow us</h2>
+            <img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="facebook icon">
+            <img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="twitter icon">
+            <img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="youtube icon">
+            <img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="pinterest icon">
+            <img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="periscope icon">
+        </div>
+    </section>
+
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,39 @@
+<header class="d-flex justify-content-between align-items-center">
+        <section class="logo">
+            <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt=" dc logo">
+        </section>
+        <section class="nav ">
+            <ul class="d-flex flex-row">
+                <li>
+                    <a href="">characters</a>
+                </li>
+                <li>
+                    <a href="">comics</a>
+                </li>
+                <li>
+                    <a href="">movies</a>
+                </li>
+                <li>
+                    <a href="">tv</a>
+                </li>
+                <li>
+                    <a href="">games</a>
+                </li>
+                <li>
+                    <a href="">collectibles</a>
+                </li>
+                <li>
+                    <a href="">videos</a>
+                </li>
+                <li>
+                    <a href="">fans</a>
+                </li>
+                <li>
+                    <a href="">news</a>
+                </li>
+                <li>
+                    <a href="">shop</a>
+                </li>
+            </ul>
+        </section>
+    </header>
